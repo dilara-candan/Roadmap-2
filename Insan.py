798524b7fe6d1e37d687914ee37ec5f1c34e6a42
dilara-candan <102910747+dilara-candan@users.noreply.github.com>
2022-04-26 14:32:55 +0300

    Add files via upload

---
+++ Insan.py
@@ -0,0 +1,32 @@
+class Insan:
+    def __init__(self):
+        self.ad = 'Dilara'
+        self.soyad = 'Candan'
+        self.yas = 25
+        self.ulke = 'Türkiye'
+        self.sehir = 'Erzurum'
+        self.yetenekler = ['Python', 'Bisiklet Binmek']
+
+    def kisi_bilgileri(self):
+        print(self.__dict__)
+
+    def yetenek_ekle(self, yetenekAdi):
+        if (type(yetenekAdi) is list):
+            self.yetenekler.extend(yetenekAdi)
+        else:
+            self.yetenekler.append(yetenekAdi)
+
+
+def main():
+    insan = Insan()
+
+    insan.yetenek_ekle('Araba Sürmek')
+    insan.yetenek_ekle('At Binmek')
+
+    # Veya
+
+    insan.yetenek_ekle(['C++', 'JavaScript'])
+
+    insan.kisi_bilgileri()
+
+main()
